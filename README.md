7e29ae0377395201dc7fa85bf0455946519a9853
zhd_nj <zhd_nj@viroyal-elec.com>
2016-08-25 17:08:34 +0800

    调试源代码，正常可以跑了

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ Logd is an open-source distributed, reliable, high-performance and available ser
 
 ## download
 please download the zip file directly.
+
+原作者 文档写的比较简陋，现补充如下：
+1、 etl 引用baidu.com/etl ，没有找到，目前暂时去掉
+2、服务分为 tail  collector monitor
+    tail 用来抓取日志
+    collector 用来收集日志 ，包含 collector ，fcollector , mgocollector ,前两个都是文件汇总日志，最后一个使用mongodb存储日志
+    monitor 监控中心，用来探测服务是否活着
+3、启动顺序
+    3.1 启动monitor : logd monitor ./conf/monitor.ini
+    3.2 启动collector : logd collector
+    3.3 启动 tail: logd tail ./conf/config.ini ，此功能只能在linux使用，必须要有tail命令
+    
+
--- runner.go
+++ runner.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-    "bytes"
-    "time"
-    "fmt"
-    "logd/heart_beat"
-    "logd/lib"
-    "logd/loglib"
-    "strconv"
-    "strings"
+	"bytes"
+	"fmt"
+	"logd/heart_beat"
+	"logd/lib"
+	"logd/loglib"
+	"strconv"
+	"strings"
+	"time"
 )
+
 func logdGo(cfg map[string]map[string]string) {
 
 	receiveChan := make(chan map[string]string)
 	sendBuffer := make(chan bytes.Buffer)
-	r := ReceiverInit(sendBuffer,receiveChan, 2000, 0)
+	r := ReceiverInit(sendBuffer, receiveChan, 2000, 0)
 
 	tc := TcpClientInit(receiveChan)
 	//start tcp listener to receive log
@@ -23,7 +24,7 @@ func logdGo(cfg map[string]map[string]string) {
 	go r.Start()
 
 	addr := "localhost:1306"
-	s := SenderInit(sendBuffer,addr, addr, 0)
+	s := SenderInit(sendBuffer, addr, addr, 0)
 	go s.Start()
 	for {
 		time.Sleep(1000 * time.Second)
@@ -31,12 +32,12 @@ func logdGo(cfg map[string]map[string]string) {
 
 }
 func tailerGo(cfg map[string]map[string]string) {
-    qlst := lib.NewQuitList()
+	qlst := lib.NewQuitList()
 
-	receiveChan := make(chan map[string]string, 10000)       //非阻塞
+	receiveChan := make(chan map[string]string, 10000) //非阻塞
 	sendBuffer := make(chan bytes.Buffer, 500)
-    recvBufferSize, _ := strconv.Atoi(cfg["tail"]["recv_buffer_size"])
-    tailler := NewTailler(cfg["tail"])
+	recvBufferSize, _ := strconv.Atoi(cfg["tail"]["recv_buffer_size"])
+	tailler := NewTailler(cfg["tail"])
 	r := ReceiverInit(sendBuffer, receiveChan, recvBufferSize, tailler.GetLineNum())
 
 	//make a new log tailler
@@ -44,90 +45,90 @@ func tailerGo(cfg map[string]map[string]string) {
 	//start receiver to receive log
 	go r.Start()
 
-    //一定要发送方先退出
-    qlst.Append(tailler.Quit)
-    qlst.Append(r.Quit)
-    // heart beat
-    port, _ := cfg["monitor"]["hb_port"]
-    monAddr, _ := cfg["monitor"]["mon_addr"]
-    if port != "" && monAddr != "" {
-        hb := heart_beat.NewHeartBeat(port, monAddr, "tail")
-        go hb.Run()
-        qlst.Append(hb.Quit)
-    }
-
-    addrs := strings.Split(cfg["tail"]["send_to"], ",")
-    addr := strings.Trim(addrs[0], " ")
-    bakAddr := addr
-    //有备用地址?
-    if len(addrs) > 1 {
-        bakAddr = strings.Trim(addrs[1], " ")
-    }
-
-    //加大发送并发，sender阻塞会影响tail的进度
-    nSenders := 2
-    senders, ok := cfg["tail"]["senders"]
-    if ok {
-        tmp, err := strconv.Atoi(senders)
-        if err == nil {
-            nSenders = tmp
-        }
-    }
-    for i:=1;i<=nSenders;i++ {
-        s := SenderInit(sendBuffer, addr, bakAddr, i)
-		go s.Start()
-        qlst.Append(s.Quit)
+	//一定要发送方先退出
+	qlst.Append(tailler.Quit)
+	qlst.Append(r.Quit)
+	// heart beat
+	port, _ := cfg["monitor"]["hb_port"]
+	monAddr, _ := cfg["monitor"]["mon_addr"]
+	if port != "" && monAddr != "" {
+		hb := heart_beat.NewHeartBeat(port, monAddr, "tail")
+		go hb.Run()
+		qlst.Append(hb.Quit)
 	}
-    loglib.Info(fmt.Sprintf("total senders %d", nSenders))
 
-    qlst.HandleQuitSignal()
-    qlst.ExecQuit()
+	addrs := strings.Split(cfg["tail"]["send_to"], ",")
+	addr := strings.Trim(addrs[0], " ")
+	bakAddr := addr
+	//有备用地址?
+	if len(addrs) > 1 {
+		bakAddr = strings.Trim(addrs[1], " ")
+	}
+
+	//加大发送并发，sender阻塞会影响tail的进度
+	nSenders := 2
+	senders, ok := cfg["tail"]["senders"]
+	if ok {
+		tmp, err := strconv.Atoi(senders)
+		if err == nil {
+			nSenders = tmp
+		}
+	}
+	for i := 1; i <= nSenders; i++ {
+		s := SenderInit(sendBuffer, addr, bakAddr, i)
+		go s.Start()
+		qlst.Append(s.Quit)
+	}
+	loglib.Info(fmt.Sprintf("total senders %d", nSenders))
+
+	qlst.HandleQuitSignal()
+	qlst.ExecQuit()
 }
 
 func collectorGo(cfg map[string]map[string]string) {
-    qlst := lib.NewQuitList()
+	qlst := lib.NewQuitList()
 
 	bufferChan := make(chan bytes.Buffer, 500)
 	rAddr := cfg["collector"]["listen"]
-	tr := TcpReceiverInit(bufferChan,rAddr)
+	tr := TcpReceiverInit(bufferChan, rAddr)
 	go tr.Start()
 
-    qlst.Append(tr.Quit)
+	qlst.Append(tr.Quit)
 
-    addrs := strings.Split(cfg["collector"]["send_to"], ",")
-    addr := addrs[0]
-    bakAddr := addr
-    //有备用地址?
-    if len(addrs) > 1 {
-        bakAddr = addrs[1]
-    }
+	addrs := strings.Split(cfg["collector"]["send_to"], ",")
+	addr := addrs[0]
+	bakAddr := addr
+	//有备用地址?
+	if len(addrs) > 1 {
+		bakAddr = addrs[1]
+	}
 
-    nSenders := 10
-    senders, ok := cfg["collector"]["senders"]
-    if ok {
-        tmp, err := strconv.Atoi(senders)
-        if err == nil {
-            nSenders = tmp
-        }
-    }
-    for i:=1;i<=nSenders;i++ {
+	nSenders := 10
+	senders, ok := cfg["collector"]["senders"]
+	if ok {
+		tmp, err := strconv.Atoi(senders)
+		if err == nil {
+			nSenders = tmp
+		}
+	}
+	for i := 1; i <= nSenders; i++ {
 		s := SenderInit(bufferChan, addr, bakAddr, i)
 		go s.Start()
-        qlst.Append(s.Quit)
+		qlst.Append(s.Quit)
 	}
-    loglib.Info(fmt.Sprintf("total senders %d", nSenders))
+	loglib.Info(fmt.Sprintf("total senders %d", nSenders))
 
-    // heart beat
-    port, _ := cfg["monitor"]["hb_port"]
-    monAddr, _ := cfg["monitor"]["mon_addr"]
-    if port != "" && monAddr != "" {
-        hb := heart_beat.NewHeartBeat(port, monAddr, "collector")
-        go hb.Run()
-        qlst.Append(hb.Quit)
-    }
+	// heart beat
+	port, _ := cfg["monitor"]["hb_port"]
+	monAddr, _ := cfg["monitor"]["mon_addr"]
+	if port != "" && monAddr != "" {
+		hb := heart_beat.NewHeartBeat(port, monAddr, "collector")
+		go hb.Run()
+		qlst.Append(hb.Quit)
+	}
 
-    qlst.HandleQuitSignal()
-    qlst.ExecQuit()
+	qlst.HandleQuitSignal()
+	qlst.ExecQuit()
 }
 
 func fcollectorGo(cfg map[string]map[string]string) {
@@ -137,80 +138,79 @@ func fcollectorGo(cfg map[string]map[string]string) {
 	fo := FileOutputerInit(bufferChan, cfg["fcollector"]["save_dir"])
 	go fo.Start()
 
-	tr := TcpReceiverInit(bufferChan,addr)
+	tr := TcpReceiverInit(bufferChan, addr)
 	go tr.Start()
-	
-    qlst := lib.NewQuitList()
 
-    // heart beat
-    port, _ := cfg["monitor"]["hb_port"]
-    monAddr, _ := cfg["monitor"]["mon_addr"]
-    if port != "" && monAddr != "" {
-        hb := heart_beat.NewHeartBeat(port, monAddr, "fcollector")
-        go hb.Run()
+	qlst := lib.NewQuitList()
 
-        qlst.Append(hb.Quit)
-    }
+	// heart beat
+	port, _ := cfg["monitor"]["hb_port"]
+	monAddr, _ := cfg["monitor"]["mon_addr"]
+	if port != "" && monAddr != "" {
+		hb := heart_beat.NewHeartBeat(port, monAddr, "fcollector")
+		go hb.Run()
 
-    qlst.Append(tr.Quit)   //tcpReceiver要先退出
-    qlst.Append(fo.Quit)
-    qlst.HandleQuitSignal()
-    qlst.ExecQuit()
+		qlst.Append(hb.Quit)
+	}
+
+	qlst.Append(tr.Quit) //tcpReceiver要先退出
+	qlst.Append(fo.Quit)
+	qlst.HandleQuitSignal()
+	qlst.ExecQuit()
 }
 
-func etlcollectorGo(cfg map[string]map[string]string) {
-    qlst := lib.NewQuitList()
+// func etlcollectorGo(cfg map[string]map[string]string) {
+//     qlst := lib.NewQuitList()
 
-	bufferChan := make(chan bytes.Buffer, 100)
-	addr := cfg["etlcollector"]["listen"]
+// 	bufferChan := make(chan bytes.Buffer, 100)
+// 	addr := cfg["etlcollector"]["listen"]
 
-	eo := EtlOutputerInit(bufferChan, cfg["etlcollector"])
-	go eo.Start()
+// 	eo := terInit(bufferChan, cfg["etlcollector"])
+// 	go eo.Start()
 
-	tr := TcpReceiverInit(bufferChan,addr)
-	go tr.Start()
-    //一定要发送方先退出
-    qlst.Append(tr.Quit)
-    qlst.Append(eo.Quit)
-	
-    // heart beat
-    port, _ := cfg["monitor"]["hb_port"]
-    monAddr, _ := cfg["monitor"]["mon_addr"]
-    if port != "" && monAddr != "" {
-        hb := heart_beat.NewHeartBeat(port, monAddr, "etlcollector")
-        go hb.Run()
-        qlst.Append(hb.Quit)
-    }
+// 	tr := TcpReceiverInit(bufferChan,addr)
+// 	go tr.Start()
+//     //一定要发送方先退出
+//     qlst.Append(tr.Quit)
+//     qlst.Append(eo.Quit)
 
-    qlst.HandleQuitSignal()
-    qlst.ExecQuit()
-}
+//     // heart beat
+//     port, _ := cfg["monitor"]["hb_port"]
+//     monAddr, _ := cfg["monitor"]["mon_addr"]
+//     if port != "" && monAddr != "" {
+//         hb := heart_beat.NewHeartBeat(port, monAddr, "etlcollector")
+//         go hb.Run()
+//         qlst.Append(hb.Quit)
+//     }
+
+//     qlst.HandleQuitSignal()
+//     qlst.ExecQuit()
+// }
 
 func mgocollectorGo(cfg map[string]map[string]string) {
-    qlst := lib.NewQuitList()
+	qlst := lib.NewQuitList()
 
-	bufferChan := make(chan bytes.Buffer, 300)   //mongodb写入并发可能开的比较高
+	bufferChan := make(chan bytes.Buffer, 300) //mongodb写入并发可能开的比较高
 	addr := cfg["mgocollector"]["listen"]
 
 	mgo := MongoDbOutputerInit(bufferChan, cfg["mgocollector"])
 	go mgo.Start()
 
-	tr := TcpReceiverInit(bufferChan,addr)
+	tr := TcpReceiverInit(bufferChan, addr)
 	go tr.Start()
-    //一定要发送方先退出
-    qlst.Append(tr.Quit)
-    qlst.Append(mgo.Quit)
-	
-    // heart beat
-    port, _ := cfg["monitor"]["hb_port"]
-    monAddr, _ := cfg["monitor"]["mon_addr"]
-    if port != "" && monAddr != "" {
-        hb := heart_beat.NewHeartBeat(port, monAddr, "mgocollector")
-        go hb.Run()
-        qlst.Append(hb.Quit)
-    }
+	//一定要发送方先退出
+	qlst.Append(tr.Quit)
+	qlst.Append(mgo.Quit)
 
-    qlst.HandleQuitSignal()
-    qlst.ExecQuit()
+	// heart beat
+	port, _ := cfg["monitor"]["hb_port"]
+	monAddr, _ := cfg["monitor"]["mon_addr"]
+	if port != "" && monAddr != "" {
+		hb := heart_beat.NewHeartBeat(port, monAddr, "mgocollector")
+		go hb.Run()
+		qlst.Append(hb.Quit)
+	}
+
+	qlst.HandleQuitSignal()
+	qlst.ExecQuit()
 }
-
